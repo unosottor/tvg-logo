c51c77dd344618511bf13a918a339481434dbb3b
DISHLIVE24 <195974522+dishlive24@users.noreply.github.com>
2025-06-12 16:47:30 +0530

    Add files via upload

---
+++ bpk-tv/api/[channel_id].py
@@ -0,0 +1,20 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route("/api/BDIX/<channel_id>.m3u8")
+def generate_m3u8(channel_id):
+    ts_url = f"http://103.120.172.74:5200/live/{channel_id}.ts"
+    m3u8_content = f"""#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-TARGETDURATION:5
+#EXT-X-MEDIA-SEQUENCE:0
+#EXTINF:5.0,
+{ts_url}
+#EXTINF:5.0,
+{ts_url}
+#EXTINF:5.0,
+{ts_url}
+#EXT-X-ENDLIST
+"""
+    return Response(m3u8_content, mimetype="application/vnd.apple.mpegurl")
